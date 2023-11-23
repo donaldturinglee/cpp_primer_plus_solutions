d75a2257ff56d410a8954efa928384fe1226e7dc
Donald Lee <donaldturinglee@gmail.com>
2023-11-23 01:09:36 -0800

    feat: chapter02 exercise03.cpp

---
+++ chapter02/exercise03.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+void f1() {
+    cout << "Three blind mice" << endl;
+}
+
+void f2() {
+    cout << "See how they run" << endl;
+}
+
+int main() {
+
+    f1();
+    f1();
+    f2();
+    f2();
+
+    return 0;
+}
