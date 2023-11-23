0a38fbe04e7f6f45c904cbfba5978a1d484ec492
Donald Lee <donaldturinglee@gmail.com>
2023-11-23 00:24:25 -0800

    feat: chapter02 exercise02.cpp

---
+++ chapter02/exercise02.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    long distance, yard;
+    cout << "Please Enter a distance: ";
+    cin >> distance;
+    yard = distance * 220;
+    cout << "Your distance is " << yard << "yard." << endl;
+    return 0;
+}
